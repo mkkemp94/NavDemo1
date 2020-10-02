a5b777d6346ccc9bbfaba13cb01b1d2801b497c7
MKemp <mkemp@accu-time.com>
2020-10-02 14:36:49 -0400

    Setup data binding

--- app/src/main/java/com/mkemp/navdemo1/MainActivity.kt
+++ app/src/main/java/com/mkemp/navdemo1/MainActivity.kt
@@ -2,12 +2,16 @@ package com.mkemp.navdemo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.mkemp.navdemo1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
 {
+    private lateinit var binding: ActivityMainBinding
+    
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
