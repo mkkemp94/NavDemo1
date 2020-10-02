04fec34624df6df94af1515abf429233ef7bdb3d
MKemp <mkemp@accu-time.com>
2020-10-02 15:11:04 -0400

    Add second fragment and provide data binding setup

---
+++ app/src/main/java/com/mkemp/navdemo1/SecondFragment.kt
@@ -0,0 +1,31 @@
+package com.mkemp.navdemo1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.mkemp.navdemo1.databinding.FragmentSecondBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SecondFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SecondFragment : Fragment() {
+
+    private lateinit var binding: FragmentSecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
+        return binding.root
+    }
+}
